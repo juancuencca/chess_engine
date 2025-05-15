f61a6e1425df4646b200f3854b100a7b2e3e6bf6
Juan Cuenca <juan99.cuenca@gmail.com>
2025-05-15 20:53:54 -0300

    add pawn and rock initialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,73 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+const ROWS: usize = 8;
+const COLS: usize = 8;
+
+trait Piece {
+
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct Pawn {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Pawn {
+    fn new() -> Pawn {
+        Pawn { }
     }
 }
+
+impl Piece for Pawn {}
+
+struct Rock {}
+
+impl Rock {
+    fn new() -> Rock {
+        Rock {  }
+    }
+}
+
+impl Piece for Rock {}
+
+struct Cell {
+    piece: Option<Box<dyn Piece>>,
+}
+
+impl Cell {
+    fn new(piece: Option<Box<dyn Piece>>) -> Cell {
+        Cell { piece }
+    }
+}
+
+pub struct Board {
+    rows: usize,
+    cols: usize,
+    cells: Vec<Cell>,
+}
+
+impl Board {
+    pub fn new() -> Board {
+        let rows = ROWS;
+        let cols = COLS;
+
+        let mut cells = Vec::with_capacity(rows * cols);
+
+        Self::init_pawns(&mut cells, cols);
+        Self::init_rocks(&mut cells, cols);
+        
+        Board { rows, cols, cells }    
+    }
+
+    fn init_rocks(cells: &mut [Cell], cols: usize) {
+        cells[0 * cols + 0] = Cell::new(Some(Box::new(Rock::new())));
+        cells[0 * cols + 7] = Cell::new(Some(Box::new(Rock::new())));
+        cells[7 * cols + 0] = Cell::new(Some(Box::new(Rock::new())));
+        cells[7 * cols + 7] = Cell::new(Some(Box::new(Rock::new())));
+    }
+
+    fn init_pawns(cells: &mut [Cell], cols: usize) {
+        for col in 0..cols {
+            cells[1 * cols + col] = Cell::new(Some(Box::new(Pawn::new())));
+            cells[6 * cols + col] = Cell::new(Some(Box::new(Pawn::new())));
+        }
+    }
+}
+
+
+
